32f7eb7fc19e253436458fe0eefb4a60754697b6
Brian Lee <brianjleeofcl@gmail.com>
2017-08-08 00:20:02 -0700

    Add score

--- src/internal/reducers/score.ts
+++ src/internal/reducers/score.ts
@@ -1,5 +1,13 @@
 import { Action } from 'redux';
 
+import { scoreAction } from '../actions';
+import { ADD_SCORE } from '../constants';
+
 export default function score(score: number = 0, action: Action): number {
+  switch(action.type) {
+    case ADD_SCORE:
+      const { amount } = action as scoreAction;
+      return score + amount;
+  }
   return score
 }
