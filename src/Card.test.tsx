8ec119fe1025c38b9357570319eac7b888fecd52
Brian Lee <brianjleeofcl@gmail.com>
2017-08-15 00:35:26 -0700

    Fix test

--- src/Card.test.tsx
+++ src/Card.test.tsx
@@ -2,7 +2,10 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Card from './Card';
 
+import { BroccoliCard } from './internal/card';
+
 it('renders without crashing', () => {
+  const card = new BroccoliCard()
   const div = document.createElement('div');
-  ReactDOM.render(<Card />, div);
+  ReactDOM.render(<Card card={card} selectDispatch={() => {}} />, div);
 });
