3d789128de34be8487757fd1da2a20740680c508
Brian Lee <brianjleeofcl@gmail.com>
2017-08-15 21:58:10 -0700

    Clean up code

--- src/internal/store.ts
+++ src/internal/store.ts
@@ -1,4 +1,4 @@
-import { createStore, Store, applyMiddleware, Middleware, Action, AnyAction } from 'redux';
+import { createStore, Store, applyMiddleware, Middleware, Action } from 'redux';
 import { State } from './state';
 import { REINIT } from './constants';
 import reducers from './combined-reducer';
@@ -11,10 +11,5 @@ const appReducers = function(state: State, action: Action) {
 
   return reducers(state, action);
 }
-
-interface createStore<S> {
-  (reducer: (state: S|undefined, action: AnyAction) => S, enhancer: ((next: (reducer: (state: S, action: AnyAction) => S, preloadedState?: S | undefined) => Store<S>) => (reducer: (state: S, action: AnyAction) => S, preloadedState?: S | undefined) => Store<S>)): Store<S>
-}
-
 
 export const store: Store<State | undefined> = createStore(appReducers, applyMiddleware(...middlewares as Middleware[]))
