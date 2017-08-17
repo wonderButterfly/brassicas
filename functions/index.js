35b3097d5614021fd28d644c014f7afd8f00413c
Brian Lee <brianjleeofcl@gmail.com>
2017-08-16 17:07:41 -0700

    Add frequency function

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,34 @@
 const functions = require('firebase-functions');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const moment = require('moment')
+
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
+
+exports.frequency = functions.https.onRequest((req, res) => {
+  const dateValues = {
+    all: null,
+    year: { years: 1 },
+    month: { months: 1 },
+    week: { weeks: 1 }
+  }
+  const { date } = req.query
+
+  if (dateValues[date] === undefined) {
+    res.status(400)
+    res.send('Invalid paramenter')
+  }
+
+  const targetTime = date === 'all' ? 0 : +moment().subtract(dateValues[date]);
+
+  admin.database().ref('/scores').orderByKey().startAt(targetTime.toString()).once('value').then(snap => {
+    const map = {};
+
+    snap.forEach(childSnap => {
+      const score = childSnap.val()
+      map[score] = (map[score] || 0) + 1; 
+    })
+    
+    res.send(map);
+  })
+})
